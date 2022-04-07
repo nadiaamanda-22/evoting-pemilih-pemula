8cace36460777e8583fe9153e2de8d1a2cb9193a
nadiaamanda-22 <nadiafebriantuy@gmail.com>
2022-04-07 21:31:13 +0700

    CRUD role udah

--- application/controllers/Role.php
+++ application/controllers/Role.php
@@ -9,7 +9,7 @@ class Role extends CI_Controller
    public function __construct()
    {
       parent::__construct();
-      $this->api = "http://localhost:4000/v2/tampilrole";
+      $this->api = "http://localhost:3000/v1/role";
    }
 
    public function index()
@@ -21,7 +21,7 @@ class Role extends CI_Controller
    {
       $api = $this->curl->simple_get($this->api);
       $decode = json_decode($api, true);
-      $apirole = $decode['values'];
+      $apirole = $decode['data'];
 
       echo json_encode($apirole);
    }
@@ -31,7 +31,7 @@ class Role extends CI_Controller
       $role =  $this->input->post('role');
 
       $ch = curl_init();
-      $url = "http://localhost:4000/tambahrole";
+      $url = "http://localhost:3000/v1/role";
       $postapi = [
          'role' => $role
       ];
@@ -66,7 +66,7 @@ class Role extends CI_Controller
       $id_role =  $this->input->post('id_role');
 
       $ch = curl_init();
-      $url = "http://localhost:4000/hapusrole";
+      $url = "http://localhost:3000/v1/role/" . $id_role;
       $postapi = [
          'id_role' => $id_role
       ];
@@ -99,50 +99,46 @@ class Role extends CI_Controller
 
    public function getIdRole($id_role)
    {
-      $this->getid = "http://localhost:4000/tampilrole/" . $id_role;
+      $this->getid = "http://localhost:3000/v1/role/" . $id_role;
       $api = $this->curl->simple_get($this->getid);
       $decode = json_decode($api, true);
-      $getid = $decode['values'];
+      $getid = $decode['data'];
 
       foreach ($getid as $data) {
       }
       echo json_encode($data);
    }
 
-   public function editRole()
+   public function editRole($idRole)
    {
+      // $id_role = $this->input->post('idrole');
       $role =  $this->input->post('role');
 
       $ch = curl_init();
-      $url = "http://localhost:4000/ubahrole";
+      $url = "http://localhost:3000/v1/role/" . $idRole;
       $postapi = [
+         'id_role' => $idRole,
          'role' => $role
       ];
       $data = json_encode($postapi);
-
-      // curl_setopt($ch, CURLOPT_URL, $url);
-      // curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'UPDATE');
-      // curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-      // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      // curl_setopt($ch, CURLOPT_POST, true);
-      // curl_setopt($ch, CURLOPT_HTTPHEADER,  array('Content-Type:application/json', 'Content-Length: ' . strlen($data)));
-      // curl_setopt($ch, CURLOPT_HEADER, 1);
-      // // curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36');
-      // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-      // // curl_setopt($ch, CURLOPT_TIMEOUT, 120);
-      // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-      // curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
-      // curl_setopt($ch, CURLOPT_TIMEOUT, 1800);
-      // $result = curl_exec($ch);
-      // curl_close($ch);
-
+      // var_dump($idRole);
+      // die;
 
       curl_setopt($ch, CURLOPT_URL, $url);
-      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'UPDATE');
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-      curl_setopt($ch, CURLOPT_POST, 1);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
       curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER,  array('Content-Type:application/json', 'Content-Length: ' . strlen($data)));
+      curl_setopt($ch, CURLOPT_HEADER, 1);
+      // curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36');
+      // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      // curl_setopt($ch, CURLOPT_TIMEOUT, 120);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
+      curl_setopt($ch, CURLOPT_TIMEOUT, 1800);
       $result = curl_exec($ch);
+      curl_close($ch);
 
       $response = [
          'sukses' => TRUE,
--- application/views/user/V_role.php
+++ application/views/user/V_role.php
@@ -91,7 +91,7 @@
                      <div class="form-group" style="margin-bottom: 20px;">
                         <label class="col-lg-4 col-sm-4 control-label">Role</label>
                         <div class="col-md-12">
-                           <input type="hidden" class="form-control" id="id_role">
+                           <input type="text" class="form-control" id="idrole" name="id_role">
                            <input type="text" class="form-control" id="namarole" name="role">
                         </div>
                      </div>
@@ -190,16 +190,17 @@
 
       //get data perid
       function getIdRole(id_role) {
+         // let id = $('#idrole').val(data.id_role);
          $.ajax({
             url: base + 'Role/getIdRole/' + id_role,
             type: 'GET',
             dataType: 'json',
             cache: false,
             success: function(data) {
-               $('#id_role').val(data.id_role);
+               $('#idrole').val(data.id_role);
                $('#namarole').val(data.role);
                $('#ubahRole').modal('show');
-               console.log(data)
+               console.log(data.id_role)
             }
          })
       }
@@ -208,11 +209,11 @@
       //edit data
       $('#ButtonEdit').click(function(e) {
          e.preventDefault()
-         let idRole = $('#id_role').val();
+         let idRole = $('#idrole').val();
          let namaRole = $('#namarole').val();
 
          $.ajax({
-            url: base + 'Role/editRole',
+            url: base + 'Role/editRole/' + idRole,
             type: 'POST',
             data: {
                id_role: idRole,
